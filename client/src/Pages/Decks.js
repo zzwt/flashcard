50999e39d691b96b4b405645e32c9aeca887a4a2
zzwt <loophone@gmail.com>
2020-07-02 20:50:34 +1000

    Display deck list.

---
+++ client/src/Pages/Decks.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect, useContext } from "react";
+import SidebarLayout from "../Components/SidebarLayout";
+import { requireAuth } from "../utils";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import AlertContext from "../Context/AlertContext/AlertContext";
+
+const Decks = () => {
+  const { setAlert } = useContext(AlertContext);
+  const [decks, setDecks] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      // You can await here
+      try {
+        const response = await axios.get("/api/decks");
+        console.log(response.data);
+        setDecks(response.data);
+      } catch (err) {
+        setAlert(err.response.data);
+      }
+    }
+    fetchData();
+  }, []);
+
+  const renderDecks = () => {
+    return decks.map((deck) => (
+      <div className="col-md-6 col-lg-4">
+        <div className="card">
+          <div className="card-body">
+            <div className="card-title">{deck.title}</div>
+            <hr></hr>
+            <div className="card-text">{deck.description}</div>
+          </div>
+        </div>
+      </div>
+    ));
+  };
+
+  return (
+    <SidebarLayout>
+      <div className="container bg-light">
+        <button className="btn btn-dark">
+          <Link to="/new-deck">Add New</Link>
+        </button>
+        <div className="row">{renderDecks()}</div>
+      </div>
+    </SidebarLayout>
+  );
+};
+
+export default requireAuth(Decks);
